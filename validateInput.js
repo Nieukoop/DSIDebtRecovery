3e0d0da741abfd15207a2c65403cd0727b273560
Leroy <leroy_nieukoop@hotmail.com>
2017-08-26 11:42:45 +0800

    input validation complete

--- validateInput.js
+++ validateInput.js
@@ -1,14 +1,33 @@
 function validate(){
-	var input = document.getElementById('name');
 
 
-if(input.value.length == 0){
-    alert("Please enter a name");
-    $("#name").css("border-color", "red");
+	var form = {inputName:["name","email","description"],
+				input:[document.getElementById("name").value,document.getElementById("email").value,
+						document.getElementById("description").value]};
 
-    setInterval(function(){ alert("Hello"); }, 3000);
-}
+	console.log(form.inputName[0] , form.input[0]);
 
+	for(var i = 0; i < form.input.length; i ++){
+		if(form.input[i].length == 0){
+			//console.log("Please enter a " + form.inputName[i]);
+			$("#"+form.inputName[i]).css("border-color", "red");
+		}
+		else{
+			$("#"+form.inputName[i]).css("border-color", "transparent");
+		}
+	}
 
+	/*if(input[x].value.length == 0)
+
+	if(input.value.length == 0){
+	    $("#name").css("border-color", "red");
+	}
+
+	input = document.getElementById('email');
+
+	if(input.value.length == 0){
+	    alert("Please enter an email");
+	    $("#name").css("border-color", "red");
+	}*/
 
 }
